08e703ecb94bf47df49d0371b4a89a4e66808418
Duke-Chapel <congregationdukechapel@gmail.com>
2025-05-12 11:35:34 -0400

    New app

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MarketingDashboard from './components/MarketingDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <MarketingDashboard />
+    </div>
+  );
+}
+
+export default App;
